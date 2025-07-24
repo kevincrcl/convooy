3f56a9de188bc926a2b9b9239264c515cd69bf37
Kevin Hanna <kevin@cassette.lol>
2025-07-24 21:38:26 +0100

    add map view

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Map, { NavigationControl } from 'react-map-gl/mapbox';
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Typography from "@mui/material/Typography";
 
-function App() {
-  const [count, setCount] = useState(0)
+const MAPBOX_TOKEN = "pk.eyJ1Ijoia2V2aW5jcmNsIiwiYSI6ImNtZGh1cjVpdjA1eHcybHNmMXZ0anlhYWsifQ.sIVlX-RHn6sTzqPf-w1VPg"; // TODO: Replace with your token
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const drawerWidth = 320;
 
-export default App
+const App: React.FC = () => {
+    return (
+        <Box sx={{ display: "flex", height: "100vh", width: "100vw" }}>
+            {/* Sidebar */}
+            <Drawer
+                variant="permanent"
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    [`& .MuiDrawer-paper`]: {
+                        width: drawerWidth,
+                        boxSizing: "border-box",
+                    },
+                }}
+            >
+                <Box sx={{ p: 2 }}>
+                    <Typography variant="h5" gutterBottom>
+                        Trip Planner
+                    </Typography>
+                    <Typography variant="body1">
+                        Stops and controls will go here.
+                    </Typography>
+                </Box>
+            </Drawer>
+            {/* Map View */}
+            <Box sx={{ flexGrow: 1, minWidth: 0 }}>
+                <Map
+                    initialViewState={{
+                        longitude: -98.5795, // Center of USA
+                        latitude: 39.8283,
+                        zoom: 4,
+                    }}
+                    style={{ width: "100%", height: "100%" }}
+                    mapStyle="mapbox://styles/mapbox/streets-v11"
+                    mapboxAccessToken={MAPBOX_TOKEN}
+                >
+                    <NavigationControl position="top-left" />
+                </Map>
+            </Box>
+        </Box>
+    );
+};
+
+export default App;
